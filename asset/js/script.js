3967c6d10782bb178923154aec7503a1fe887314
sherlyls <sherlyls862@gmail.com>
2023-10-23 17:18:21 +0700

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,21 +1,22 @@
-$(document).ready(function() {
-    $('.icon-burger').click(function() {
-      $(this).siblings(".content-burger").toggle();
-    });
+$(document).ready(function () {
+  $('.icon-burger').click(function () {
+    $(this).siblings(".content-burger").toggle();
   });
-
-  $(document).ready(function() {
-    $('.search .icon').on("click",function(){
-      $(".box-search form input").css("display","block")
-    });
+  $('.other .search .icon').on('click', function () {
+    $('.box-search').toggleClass('open');
   });
+  $('.box-search .close').on('click', function () {
+    $('.box-search').removeClass('open');
+  });
+});
 
-  $(window).scroll(function() {
 
-    if ($(this).scrollTop() > 10){  
-        $('header').addClass("sticky");
-    }
-    else{
-        $('header').removeClass("sticky");
-    }
+$(window).scroll(function () {
+
+  if ($(this).scrollTop() > 10) {
+    $('header').addClass("sticky");
+  }
+  else {
+    $('header').removeClass("sticky");
+  }
 });
