8d4bc08f135de728be900bb4763ed86131be132e
sherlyls <sherlyls862@gmail.com>
2023-10-24 17:41:18 +0700

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-  $('.icon-burger').click(function () {
-    $(this).siblings(".content-burger").toggle();
+  $('.icon-burger').on('click', function () {
+    $('.bg-burger').toggleClass('open');
+  });
+  $('.bg-burger').on('click', function () {
+    $('.bg-burger').removeClass('open');
+  }).children().click(function (e) {
+    return false;
   });
   $('.other .search .icon').on('click', function () {
     $('.box-search').toggleClass('open');
@@ -21,3 +26,13 @@ $(window).scroll(function () {
 });
 
 function toggle_visibility(e) { var l = document.getElementById(e); "none" == l.style.display ? l.style.display = "block" : l.style.display = "none" } document.querySelector(".rec-x").onclick = function () { document.querySelector("#show-hover").style.display = "none" };
+
+if ($("#size-font").length) {
+  for (var e, t = document.getElementById("size-font").getElementsByClassName("font"), i = 0; i < t.length; i++)
+      t[i].addEventListener("click", function () {
+          var e = document.getElementsByClassName("active");
+          (e[0].className = e[0].className.replace("active", "")), (this.className += " active");
+      }),
+          (document.getElementById("size-font").style.display = "flex");
+  document.getElementById("size-font").style.display = "flex";
+}
